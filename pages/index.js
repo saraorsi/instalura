fb7347707ed4444464405b3afcf8a935d341f1cb
Sara Orsi <sararaorsi@gmail.com>
2021-02-09 15:31:26 +0000

    aula 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const Title = styled.h1`
+//   font-size: 50px;
+//   color: ${({ theme }) => theme.colors.primary};
+// `
+
+const Title = (props) => <h1>{props.children}</h1>
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>Olá</Title>
 }
